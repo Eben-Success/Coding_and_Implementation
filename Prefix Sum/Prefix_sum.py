bd6a4fc82f6977cede00be8c129c306a0db6856e
Eben-Success <aebenezer237@gmail.com>
2022-11-09 23:09:22 +0000

    Implementation of queue DSA

--- Prefix Sum/Prefix_sum.py
+++ Prefix Sum/Prefix_sum.py
@@ -1,14 +1,15 @@
-def fillPrefixSum(arr, n , prefixSum):
+def fillPrefixSum(arr, n, prefixSum):
     prefixSum[0] = arr[0]
 
     for i in range(1, n):
-        prefixSum[i] = prefixSum[i-1] + arr[i]
+        prefixSum[i] = prefixSum[i - 1] + arr[i]
+
 
 arr = [10, 4, 16, 20]
 n = len(arr)
-prefixSum = [0 for i in range(n+1)]
+prefixSum = [0 for i in range(n + 1)]
 
-fillPrefixSum(arr, n , prefixSum)
+fillPrefixSum(arr, n, prefixSum)
 
 for i in range(n):
     print(prefixSum[i], " ", end="")
