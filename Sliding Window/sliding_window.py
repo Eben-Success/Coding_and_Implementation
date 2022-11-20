c23bd76085d768a3cc98fd52d219160d167199c2
Eben-Success <aebenezer237@gmail.com>
2022-11-20 08:40:00 +0000

    Implementation of sliding window

--- Sliding Window/sliding_window.py
+++ Sliding Window/sliding_window.py
@@ -1,11 +1,13 @@
 def sliding_window(arr, k):
+    # Sum up the first subarray and add it to the result
     curr_subarray = sum(arr[:k])
     result = [curr_subarray]
 
-    for i in range(1, len(arr)-k+1):
+    # To get each subsequent subarray, add the next value in
+    # the list and remove the first  value.
+    for i in range(1, len(arr) -k + 1):
         curr_subarray = curr_subarray - arr[i-1]
-        curr_subarray = curr_subarray - arr[i+k-1]
-
+        curr_subarray = curr_subarray + arr[i+k-1]
         result.append(curr_subarray)
 
     return result
