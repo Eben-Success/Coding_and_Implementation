c5bc3d409157d86b7f7449d9b1b1c492982f522e
Eben-Success <aebenezer237@gmail.com>
2022-11-20 08:28:08 +0000

    Implementation of queue DSA

--- InsertionSort/InsertionSort.py
+++ InsertionSort/InsertionSort.py
@@ -3,9 +3,9 @@ Insertion sort is a sorting algorithm that places an unsorted element at its sui
 in each iteration.
 """
 
-def insertionSort(array):
 
-    for step in range(1, len(array)):
+def insertionSort(array):
+    for step in range(len(array)):
         key = array[step]
         j = step - 1
 
@@ -13,14 +13,14 @@ def insertionSort(array):
         # element smaller than it is found.
 
         while j >= 0 and key < array[j]:
-            array[j+1] = array[j]
+            array[j + 1] = array[j]
             j = j - 1
 
         # Place key after the element just smaller than it
-        array[j+1] = key
+        array[j + 1] = key
 
 
-data = [9, 5, 1, 4, 3]
+data = [9, 5, 1, 4, 3, -2, -34, 43, 3434]
 insertionSort(data)
 print("Sorted Array in Ascending Order: ")
 print(data)
