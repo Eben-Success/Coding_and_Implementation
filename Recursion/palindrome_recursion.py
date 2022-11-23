c3176a059f6ba955ec7f0d64bf74aacc677aa7b8
Eben-Success <aebenezer237@gmail.com>
2022-11-23 15:57:44 +0000

    update

---
+++ Recursion/palindrome_recursion.py
@@ -0,0 +1,8 @@
+# Recursion
+def isPalindrome(word):
+    if len(word) < 2: return True
+    if word[0] != word[-1]: return False
+    return isPalindrome(word[1:-1])
+
+word = "kayak"
+print(isPalindrome(word))
