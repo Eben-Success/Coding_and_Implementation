41867f248865288c94694ca98d4c532699f35f12
Eben-Success <aebenezer237@gmail.com>
2022-11-30 11:43:55 +0000

    update

---
+++ Stacks/stack_using_queues.py
@@ -0,0 +1,15 @@
+# Implement Stack using Queues
+
+# BRUTE FORCE APPROAH
+"""Steps:
+push(x)
+1. Add x to Q2
+2. Move Q1 to Q2 / Element by element
+3. swap Q1 and Q2"""
+
+# Time complexity is O(n)
+# Space complexity is O(n)
+
+#OPTIMAL APPROAH
+# find the len(stack) - 1 and remove and add it to the stack again.
+
