56b86f8cc7d8b034aafb60785e276407247f750f
Eben-Success <aebenezer237@gmail.com>
2022-11-30 09:33:56 +0000

    update

--- InsertionSort/InsertionSort.py
+++ InsertionSort/InsertionSort.py
@@ -1,6 +1,5 @@
 """
-Insertion sort is a sorting algorithm that places an unsorted element at its suitable place
-in each iteration.
+Insertion sort is a sorting algorithm that places an unsorted element at its suitable place at each iteration.
 """
 
 
