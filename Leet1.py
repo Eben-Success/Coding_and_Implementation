0bf04680815b2e2fff0e6f9f59583089d2ffefd1
Eben-Success <aebenezer237@gmail.com>
2022-12-03 16:56:42 +0000

    linked list

--- Leet1.py
+++ Leet1.py
@@ -1,18 +1,35 @@
-def removeKdigit(num, k):
-    
-    stack = []
-    
-    for c in num:
-        while k > 0 and stack and stack[-1]  > c:
-            k -= 1
-            stack.pop()
-        stack.append(c)
-    
-    stack = stack[:len(stack) - k]
-    res = "".join(stack)
-    return str(int(res)) if res else "0"
+def spiralOrder(matrix):
+    res = []
+    left, right = 0, len(matrix[0])
+    top, bottom = 0, len(matrix)
 
+    while left < right and top < bottom:
 
-num = "10200"
+        # get every i in the top row
+        for i in range(left, right):
+            res.append(matrix[top][i])
+        top +=1
 
-print(removeKdigit(num, k=3))
+        # get every i in the right col
+        for i in range(top, bottom):
+            res.append(matrix[i][right - 1])
+        right -= 1
+
+        if not (left < right and top < bottom):
+            break
+
+        # get every i in the bottom row
+        for i in range(right - 1, left - 1, -1):
+            res.append(matrix[bottom - 1][i])
+        bottom -= 1
+
+        # get every i in the left most column
+        for i in range(bottom-1, top-1, -1):
+            res.append(matrix[i][left])
+        left += 1
+    return res
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+print(spiralOrder(matrix))
+
