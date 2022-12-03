839bb8decd700aecfb8204a3aa77097c91e627d4
Eben-Success <aebenezer237@gmail.com>
2022-12-03 16:57:09 +0000

    linked list

---
+++ Binary_Search_Leetcode/Number_floor.py
@@ -0,0 +1,18 @@
+def floor(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return high
+
+arr = [2,3,5,9,14,16,18]
+
+print(floor(arr, 1))
