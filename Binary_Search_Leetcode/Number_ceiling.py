b99be234c51e2a54bc8e24308a1b71ea13688be3
Eben-Success <aebenezer237@gmail.com>
2022-12-03 16:56:58 +0000

    linked list

---
+++ Binary_Search_Leetcode/Number_ceiling.py
@@ -0,0 +1,23 @@
+# find the smallest element in array >= target element.
+
+def ceiling_number(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    if target > high:
+        return -1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low
+
+arr = [2,3,5,9,14,16,18]
+
+print(ceiling_number(arr, 19))
