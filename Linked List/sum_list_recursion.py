f42f2bea09b2d36728e1d19e6075768e59642f77
Eben-Success <aebenezer237@gmail.com>
2022-12-03 18:21:23 +0000

    linked list

--- Linked List/sum_list_recursion.py
+++ Linked List/sum_list_recursion.py
@@ -1,15 +1,17 @@
 # Time O(n)
-# Space O(1)
+# Space O(n): Coz of the call stack
 
 class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
 
+
 def sum_list(head):
     if head is None:
         return 0
     return head.val + sum_list(head.next)
+
 
 x = Node(38)
 y = Node(4)
@@ -17,4 +19,3 @@ y = Node(4)
 x.next = y
 
 print(sum_list(x))
-
