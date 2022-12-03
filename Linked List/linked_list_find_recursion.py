c22c223e7b82f24e44647bdf8370bf20e7adfbd9
Eben-Success <aebenezer237@gmail.com>
2022-12-03 18:23:58 +0000

    linked list

--- Linked List/linked_list_find_recursion.py
+++ Linked List/linked_list_find_recursion.py
@@ -3,3 +3,22 @@ class Node:
         self.val = val
         self.next = None
 
+def linked_list_find(head, target):
+    if head is None:
+        return False
+    if head.val == target:
+        return True
+    return linked_list_find(head.next, target)
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+print(linked_list_find(a, "b"))
+
