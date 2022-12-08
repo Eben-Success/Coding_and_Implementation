ad6e8bd85eb306b78b492e83fa4c4348b01938a6
Eben-Success <aebenezer237@gmail.com>
2022-12-08 14:29:16 +0000

    linked list

--- Leet1.py
+++ Leet1.py
@@ -1,35 +1,27 @@
-def spiralOrder(matrix):
+from heapq import heappushpop, heappush
+
+
+def topKFrequent(nums, k):
+    
     res = []
-    left, right = 0, len(matrix[0])
-    top, bottom = 0, len(matrix)
+    dic = {}
+    
+    for num in nums:
+        if num not in dic:
+            dic[num] = 1
+        else:
+            dic[num] += 1
+            
+    for key, val in dic.items():
+        if len(res) < k:
+            heappush(res, [val, key])
+        else:
+            heappushpop(res, [val, key])
+    return [key for value, key in res]
 
-    while left < right and top < bottom:
+    
 
-        # get every i in the top row
-        for i in range(left, right):
-            res.append(matrix[top][i])
-        top +=1
+nums = [1,1,1,2,2,3]
+k = 2
 
-        # get every i in the right col
-        for i in range(top, bottom):
-            res.append(matrix[i][right - 1])
-        right -= 1
-
-        if not (left < right and top < bottom):
-            break
-
-        # get every i in the bottom row
-        for i in range(right - 1, left - 1, -1):
-            res.append(matrix[bottom - 1][i])
-        bottom -= 1
-
-        # get every i in the left most column
-        for i in range(bottom-1, top-1, -1):
-            res.append(matrix[i][left])
-        left += 1
-    return res
-
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-
-print(spiralOrder(matrix))
-
+print(topKFrequent(nums, k))
