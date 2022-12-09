0ff6269f4bd14c3109ce0f8af8f0dae77f184194
Eben-Success <aebenezer237@gmail.com>
2022-12-09 15:50:12 +0000

    linked list

---
+++ Binary Tree/max_root_to_leaf_path_sum.py
@@ -0,0 +1,54 @@
+# max-root-to-leaf-path-sum
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def max_path_sum(root):
+    if root is None:
+        return - float("inf")
+    if root.left is None and root.right is None:
+        return root.val
+    max_child = max(max_path_sum(root.left),
+                    max_path_sum(root.right))
+    return root.val + max_child
+
+
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(2)
+f = Node(4)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+print(max_path_sum(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Leet2.py
+++ Leet2.py
@@ -1,4 +1,8 @@
-dic = {}
-nums = [1,2,3,4,5,6,3,1]
-nums.count(1)
-print(nums)
+from collections import defaultdict
+
+nums = [2,2,4,4,5,5,6,6,7,8,7]
+
+check = defaultdict(set)
+
+for idx, num in enumerate(nums):
+    check.append(idx, num)
