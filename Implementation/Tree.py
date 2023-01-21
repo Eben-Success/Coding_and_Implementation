0ec30a681c2afc6787d3418f102a1c216bc34973
Eben-Success <aebenezer237@gmail.com>
2023-01-21 09:08:07 +0000

    update

---
+++ Implementation/Tree.py
@@ -0,0 +1,27 @@
+class BinaryTree:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+    def insert_left(self, val):
+        pass
+
+    def insert_right(self, val):
+        pass
+
+    def insert_node(self, val):
+        pass
+
+    def find_node(self, val):
+        pass
+
+    def remove_node(self, val, parent):
+        pass
+
+    def clear_node(self):
+        pass
+
+    def find_minimum_value(self):
+        pass
