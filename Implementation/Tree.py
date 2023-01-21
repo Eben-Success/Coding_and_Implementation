f1d54ccf89c55845ff0603d3c91f3d96549247a9
Eben-Success <aebenezer237@gmail.com>
2023-01-21 13:51:12 +0000

    update

--- Implementation/Tree.py
+++ Implementation/Tree.py
@@ -71,17 +71,18 @@ class BST:
 
     # delete a node from the BST
     def delete_node(self, val):
-
         """
+        Base case: if the node is empty.
         Edge cases:
-        1. The node has no children.
-        2. The node has one child.
+        1. The node has on child
+        2. The node has one child
         3. The node has two children.
         """
 
         self.root = self._delete_node(self.root, val)
 
-
+    
+    # private method to delete node
     def _delete_node(self, node, val):
         if not node:
             return None
@@ -90,6 +91,44 @@ class BST:
             node.left = self._delete_node(node.left, val)
         elif val > node.val:
             node.right = self._delete_node(node.right, val)
+
+        else:
+            # if node has no child
+
+            if not node.left and not node.right:
+                return None
+
+            # if node has one child
+            elif not node.left:
+                return node.right
+
+            elif not node.right:
+                return node.left
+
+            else:
+                # the node has 2 children
+                """
+                In-order successor is used when deleting a node with 2 children.
+                In an in-order traversal, the nodes are visited in increasing order, 
+                so the in-order successor is the next node that would be visited 
+                in the traversal.
+                By replacing the node to be deleted with its in-order successor, 
+                we ensure that the ordering property of BST is maintained.
+                """
+
+                in_order_successor = self._find_min(node.right)
+                node_val = in_order_successor.val
+                node.right = self._delete_node(node.right, in_order_successor)
+
+        return node
+
+    def _find_min(self, node):
+        while node.left:
+            node = node.left
+        return node
+
+
+        
 
 
 
