db5917704a1ed8b21bada77cb22fdd5f6c860a9d
Eben-Success <aebenezer237@gmail.com>
2023-01-27 22:03:03 +0000

    update

---
+++ Google Prep/Graphs/rotting_oranges.py
@@ -0,0 +1,20 @@
+# Time: O(r * c)
+# Space Complexity: O(r * C)
+
+"""
+0: empty cell
+1. fruit
+2. rotten oranges
+
+converting fruits(1s) to rotten oranges(2s) and return 
+the minimum time it takes to do that else return -1
+"""
+
+
+def rotten_orange(grid):
+    if not grid:
+        return
+
+    
+
+    
