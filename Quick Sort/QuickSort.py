7c0fb32214d3caa899fa72c2d8be32bd61d7f046
Eben-Success <aebenezer237@gmail.com>
2023-01-31 11:56:06 +0000

    update

--- Quick Sort/QuickSort.py
+++ Quick Sort/QuickSort.py
@@ -14,3 +14,14 @@ def partition(array, low, high):
 
     # swap pivot with element e greater than specified i
     array[i+1], array[high] = array[high], array[i+1]
+
+    return i + 1
+
+
+def quicksort(array, low, high):
+
+    if low < high:
+        pi = partition(array, low, high)
+
+        quicksort(array, low, pi - 1)
+        quicksort(array, pi+1, high)
