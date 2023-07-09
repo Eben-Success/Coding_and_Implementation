db8268bf010bd2553b2400f1eb3701b95d38c163
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-07-09 15:41:27 +0000

    created revision file for linkedlist

---
+++ LinkedList/revision/linkedlist_revision.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+        
+    
+        
+
+        
+        
